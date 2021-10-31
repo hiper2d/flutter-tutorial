ee1c6e2443486562b58ce91913fecbfa852ed2c9
hiper2d <hiper2d@gmail.com>
2021-10-31 16:23:28 -0400

    Lesson 15: Created ID Card page

--- lib/main.dart
+++ lib/main.dart
@@ -11,44 +11,82 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hey'),
+        title: const Text('ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              color: Colors.cyan,
-              padding: const EdgeInsets.all(30.0),
-              child: const Text('1'),
+      backgroundColor: Colors.grey[900],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpeg'),
+                radius: 40.0,
+              )
             ),
-            flex: 3,
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.pinkAccent,
-              padding: const EdgeInsets.all(30.0),
-              child: const Text('2'),
+            const Divider(
+              height: 60.0,
+              color: Color(0xFF424242), // grey[800],
             ),
-            flex: 2,
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.amber,
-              padding: const EdgeInsets.all(30.0),
-              child: const Text('3'),
+            const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
             ),
-            flex: 1,
-          ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: const Text('click'),
-        backgroundColor: Colors.red,
+            const SizedBox(height: 10.0),
+            const Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              )
+            ),
+            const SizedBox(height: 30.0),
+            const Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            const SizedBox(height: 10.0),
+            const Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              )
+            ),
+            const SizedBox(height: 30.0),
+            Row(
+              children: const [
+                Icon(
+                  Icons.email,
+                  color: Color(0xFFBDBDBD), // grey[400]
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'chun.li@ninjas.com',
+                  style: TextStyle(
+                    color: Color(0xFFBDBDBD), // grey[400]
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
       )
     );
   }
