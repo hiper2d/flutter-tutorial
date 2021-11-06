33fb4ad99f130cb874c06450548fc4bb4cf146cc
hiper2d <hiper2d@gmail.com>
2021-11-06 17:49:08 -0400

    Lesson 17: Played with list of text widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
-  home: Home(),
+  home: QuoteList(),
 ));
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
+class _QuoteListState extends State<QuoteList> {
 
-  int sealLevel = 0;
+  List<String> quotes = [
+    'Be yourself; everyone is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('ID Card'),
+        backgroundColor: Colors.redAccent,
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
+        title: const Text('Awesome Quotes'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            sealLevel += 1;
-          });
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
-      backgroundColor: Colors.grey[900],
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/snuffy.jpeg'),
-                radius: 40.0,
-              )
-            ),
-            const Divider(
-              height: 60.0,
-              color: Color(0xFF424242), // grey[800],
-            ),
-            const Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              )
-            ),
-            const SizedBox(height: 10.0),
-            const Text(
-              'Snuffy',
-              style: TextStyle(
-                color: Colors.amberAccent,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-              )
-            ),
-            const SizedBox(height: 30.0),
-            const Text(
-              'CURRENT SEAL LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              )
-            ),
-            const SizedBox(height: 10.0),
-            Text(
-              '$sealLevel',
-              style: const TextStyle(
-                color: Colors.amberAccent,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-              )
-            ),
-            const SizedBox(height: 30.0),
-            Row(
-              children: const [
-                Icon(
-                  Icons.email,
-                  color: Color(0xFFBDBDBD), // grey[400]
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'snuffy@seal.com',
-                  style: TextStyle(
-                    color: Color(0xFFBDBDBD), // grey[400]
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                )
-              ],
-            ),
-          ],
-          crossAxisAlignment: CrossAxisAlignment.start,
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes.map((q) => Text(q)).toList(),
       )
     );
   }
---
+++ lib/seal.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class SealIdCard extends StatefulWidget {
+  const SealIdCard({Key? key}) : super(key: key);
+
+  @override
+  State<SealIdCard> createState() => _SealIdCardState();
+}
+
+class _SealIdCardState extends State<SealIdCard> {
+
+  int sealLevel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              sealLevel += 1;
+            });
+          },
+          child: const Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+        ),
+        backgroundColor: Colors.grey[900],
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            children: <Widget>[
+              const Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/snuffy.jpeg'),
+                    radius: 40.0,
+                  )
+              ),
+              const Divider(
+                height: 60.0,
+                color: Color(0xFF424242), // grey[800],
+              ),
+              const Text(
+                  'NAME',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  )
+              ),
+              const SizedBox(height: 10.0),
+              const Text(
+                  'Snuffy',
+                  style: TextStyle(
+                    color: Colors.amberAccent,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  )
+              ),
+              const SizedBox(height: 30.0),
+              const Text(
+                  'CURRENT SEAL LEVEL',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  )
+              ),
+              const SizedBox(height: 10.0),
+              Text(
+                  '$sealLevel',
+                  style: const TextStyle(
+                    color: Colors.amberAccent,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  )
+              ),
+              const SizedBox(height: 30.0),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.email,
+                    color: Color(0xFFBDBDBD), // grey[400]
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'snuffy@seal.com',
+                    style: TextStyle(
+                      color: Color(0xFFBDBDBD), // grey[400]
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  )
+                ],
+              ),
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+        )
+    );
+  }
+}
