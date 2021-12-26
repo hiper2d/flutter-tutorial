53ede69fa7f1c724047a7b91149ff0b74071ff7c
hiper2d <hiper2d@gmail.com>
2021-12-25 22:46:03 -0500

    Flutter Clean Code: Lesson 10: Added NumberTrivia states for BLOC

--- lib/trivia/feature/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/trivia/feature/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
+import 'dart:html';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_test3/trivia/feature/number_trivia/domain/entity/number_trivia.dart';
 import 'package:meta/meta.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  NumberTriviaBloc() : super(Empty()) {
     on<NumberTriviaEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/trivia/feature/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/trivia/feature/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -2,13 +2,26 @@ part of 'number_trivia_bloc.dart';
 
 @immutable
 abstract class NumberTriviaState extends Equatable {
+  final List<dynamic> properties;
 
-  const NumberTriviaState([List props = const <dynamic>[]]);
+  const NumberTriviaState([this.properties = const <dynamic>[]]);
 
   @override
-  List<Object> get props {
-    return props;
-  }
+  List<dynamic> get props => properties;
 }
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({required this.trivia}): super([trivia]);
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({required this.message}): super([message]);
+}
