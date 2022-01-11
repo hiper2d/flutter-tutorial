1367b73dd658a0b434badf1bb83b900c36669271
hiper2d <hiper2d@gmail.com>
2022-01-10 22:53:34 -0500

    Flutter Clean Code: Lesson 13: Added dependency injection

---
+++ lib/trivia/injection_container.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test3/trivia/core/network/network_info.dart';
+import 'package:flutter_test3/trivia/feature/number_trivia/data/datasource/number_trivia_remote_datasource.dart';
+import 'package:flutter_test3/trivia/feature/number_trivia/domain/usecase/get_concrete_number_trivia.dart';
+import 'package:flutter_test3/trivia/feature/number_trivia/domain/usecase/get_random_number_trivia.dart';
+import 'package:flutter_test3/trivia/feature/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/util/input_converter.dart';
+import 'feature/number_trivia/data/datasource/number_trivia_local_datasource.dart';
+import 'feature/number_trivia/data/repository/number_trivia_repository_impl.dart';
+import 'feature/number_trivia/domain/repository/number_trivia_repository.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Bloc
+  sl.registerFactory(
+      () => NumberTriviaBloc(getConcreteTriviaUseCase: sl(), getRandomTriviaUseCase: sl(), inputConverter: sl()));
+
+  // Use Cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(repository: sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(repository: sl()));
+
+  // Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+      () => NumberTriviaRepositoryImpl(localDataSource: sl(), remoteDataSource: sl(), networkInfo: sl()));
+
+  //Data sources
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(() => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()));
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(() => NumberTriviaRemoteDataSourceImpl(client: sl()));
+
+  // Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
--- lib/trivia/trivia_main.dart
+++ lib/trivia/trivia_main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'injection_container.dart' as di;
 
-void main() => runApp(const MaterialApp(
-  home: Text('Awesome'),
-));
+void main() async {
+  await di.init();
+  runApp(const MaterialApp(
+    home: Text('Awesome'),
+  ));
+}
